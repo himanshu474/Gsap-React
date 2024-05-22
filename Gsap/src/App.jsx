1581421a28fe9458143862567e27b7a32ed71b55
Himanshu474 <himanshurawat474@gmail.com>
2024-05-23 03:15:33 +0530

    All done

--- Gsap/src/App.jsx
+++ Gsap/src/App.jsx
@@ -1,34 +1,33 @@
-import React from "react";
-import Header from "./component/Header";
-import Main from "./component/Main";
-import Paragraph from "./component/Paragraph";
-import Rating from "./component/Rating";
-import Button from "./component/Button";
-import SectionPage from "./component/SectionPage";
-import CardImage from "./component/CardImage";
-import Plugins from "./component/Plugins";
-import Footer from "./component/Footer";
-import GsapAnimation from "./GsapAnimation";
-
-
+import React from 'react'
+import Header from './component/Header'
+import Main from './component/Main'
+import Paragraph from './component/Paragraph'
+import Rating from './component/Rating'
+import Button from './component/Button'
+import SectionPage from './component/SectionPage'
+import CardImage from './component/CardImage'
+import Plugins from './component/Plugins'
+import Footer from './component/Footer'
+import GsapAnimation from "./GsapAnimation"
 
 const App = () => {
+
+  
   return (
     <>
-      <GsapAnimation>
-        
-          <Header />
-          <Main />
-          <Paragraph />
-          <Rating />
-          <Button />
-          <SectionPage />
-          <CardImage />
-          <Plugins />
-          <Footer />
-        </GsapAnimation>
-    </>
-  );
-};
+      <GsapAnimation/>      
+      <Header/>
+      <Main/>
+      <Paragraph/>
+      <Rating/>
+      <Button/>
+      <SectionPage/>
+      <CardImage/>
+      <Plugins/>
+      <Footer/>
+      
+      </>
+  )
+}
 
-export default App;
+export default App
--- Gsap/src/GsapAnimation.jsx
+++ Gsap/src/GsapAnimation.jsx
@@ -1,97 +1,147 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-const GsapAnimation = ({children}) => {
-    const canvasRef = useRef(null);
-    const particlesArray = [];
-    const colors = [
-        'rgba(255, 0, 0, 0.2)',
-        'rgba(0, 255, 0, 0.2)',
-        'rgba(0, 0, 255, 0.2)',
-        'rgba(255, 255, 0, 0.2)',
-        'rgba(0, 255, 255, 0.2)'
+const GsapAnimatiom = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    let w,
+      h,
+      balls = [];
+    let mouse = { x: undefined, y: undefined };
+    let rgb = [
+      "rgba(26, 188, 156, 0.5)",
+      "rgba(46, 204, 113, 0.5)",
+      "rgba(52, 152, 219, 0.5)",
+      "rgba(155, 89, 182, 0.5)",
+      "rgba(241, 196, 15, 0.5)",
+      "rgba(230, 126, 34, 0.5)",
+      "rgba(231, 76, 60, 0.5)",
     ];
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
+    const resizeReset = () => {
+      w = canvas.width = window.innerWidth;
+      h = canvas.height = window.innerHeight;
+    };
 
-        const handleResize = () => {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
+    const animationLoop = () => {
+      ctx.clearRect(0, 0, w, h);
+      ctx.globalCompositeOperation = "lighter";
+      drawBalls();
+
+      let temp = [];
+      for (let i = 0; i < balls.length; i++) {
+        if (balls[i].time <= balls[i].ttl) {
+          temp.push(balls[i]);
+        }
+      }
+      balls = temp;
+
+      requestAnimationFrame(animationLoop);
+    };
+
+    const drawBalls = () => {
+      for (let i = 0; i < balls.length; i++) {
+        balls[i].update();
+        balls[i].draw();
+      }
+    };
+
+    const mousemove = (e) => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+
+      for (let i = 0; i < 3; i++) {
+        balls.push(new Ball());
+      }
+    };
+
+    const mouseout = () => {
+      mouse.x = undefined;
+      mouse.y = undefined;
+    };
+
+    const getRandomInt = (min, max) => {
+      return Math.round(Math.random() * (max - min)) + min;
+    };
+
+    const easeOutQuart = (x) => {
+      return 1 - Math.pow(1 - x, 4);
+    };
+
+    class Ball {
+      constructor() {
+        this.start = {
+          x: mouse.x + getRandomInt(-20, 20),
+          y: mouse.y + getRandomInt(-20, 20),
+          size: getRandomInt(110, 120),
+        };
+        this.end = {
+          x: this.start.x + getRandomInt(-300, 300),
+          y: this.start.y + getRandomInt(-300, 300),
         };
 
-        const handleParticles = () => {
-            for (let i = 0; i < particlesArray.length; i++) {
-                particlesArray[i].update();
-                particlesArray[i].draw(ctx);
-                if (particlesArray[i].size <= 0.5) {
-                    particlesArray.splice(i, 1);
-                    i--;
-                }
-            }
-        };
+        this.x = this.start.x;
+        this.y = this.start.y;
+        this.size = this.start.size;
 
-        const createParticle = (x, y) => {
-            const size = Math.random() * 20 + 10;
-            const color = colors[Math.floor(Math.random() * colors.length)];
-            const speedX = Math.random() * 3 - 1.5;
-            const speedY = Math.random() * 3 - 1.5;
+        this.style = rgb[getRandomInt(0, rgb.length - 1)];
 
-            return {
-                x,
-                y,
-                size,
-                color,
-                speedX,
-                speedY,
-                update() {
-                    this.x += this.speedX;
-                    this.y += this.speedY;
-                    this.size *= 0.95; // Gradually shrink the particle
-                },
-                draw(ctx) {
-                    ctx.fillStyle = this.color;
-                    ctx.beginPath();
-                    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-                    ctx.fill();
-                }
-            };
-        };
+        this.time = 0;
+        this.ttl = 100;
+      }
+      draw() {
+        ctx.fillStyle = this.style;
+        ctx.shadowBlur = 15;
+        ctx.shadow = this.style;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+      }
+      update() {
+        if (this.time <= this.ttl) {
+          let progress = 1 - (this.ttl - this.time) / this.ttl;
 
-        const handleMouseMove = (event) => {
-            const { clientX, clientY } = event;
-            for (let i = 0; i < 5; i++) {
-                particlesArray.push(createParticle(clientX, clientY));
-            }
-        };
+          this.size = this.start.size * (1 - easeOutQuart(progress));
+          this.x = this.x + (this.end.x - this.x) * 0.01;
+          this.y = this.y + (this.end.y - this.y) * 0.01;
+          this.style = this.style.replace(
+            /[\\.]+\)$/g,
+            `${0.5 * (1 - progress)})`
+          );
+        }
+        this.time++;
+      }
+    }
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        window.addEventListener('resize', handleResize);
-        canvas.addEventListener('mousemove', handleMouseMove);
+    const init = () => {
+      resizeReset();
+      animationLoop();
+    };
 
-        const animate = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            handleParticles();
-            requestAnimationFrame(animate);
-        };
+    window.addEventListener("resize", resizeReset);
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseout", mouseout);
+    init();
 
-        animate();
+    return () => {
+      window.removeEventListener("resize", resizeReset);
+      window.removeEventListener("mousemove", mousemove);
+      window.removeEventListener("mouseout", mouseout);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            canvas.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, []);
+  return (
+    <canvas
+      ref={canvasRef}
+      className="fixed hidden lg:block inset-0 -z-10 bg-transparent w-full h-full"
+    />
+  );
+};
 
-return(
-<div style={{ position: 'relative', width: '100%', height: '100%' }}>
-            <canvas ref={canvasRef} style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }} />
-            {children}
-        </div>
-
-)
-}
-
-
-export default GsapAnimation;
+export default GsapAnimatiom;
